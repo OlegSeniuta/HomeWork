b84ac31c9bb43ea2f25773d2adcf037f0cca09ca
OlegSeniuta <olegvemo@gmail.com>
2021-05-19 23:53:10 -0400

    commit1

---
+++ lesson_1/task2.py
@@ -0,0 +1,18 @@
+# Hello
+# world
+# of
+# tomorrow!
+print("Hello", "world", "of", "tomorrow", sep="\n", end="!\n")
+# Hello world of tomorrow!
+t = "Hello"
+a = "world"
+s = "of"
+k = "tomorrow!"
+print(t, a, s, k, sep="\t")
+#
+n = 28
+sep = " "
+# Hello world number 28
+print(t + sep + a + sep + "number" + sep + str(n))
+# -Hello !
+print(f"-{t} ! ")
---
+++ lesson_1/task3.py
@@ -0,0 +1,4 @@
+a = '#' * 9
+b = '#\t\t#'
+print(a, b, b, b, a, sep='\n', end='\n\n\n')
+print(b, b, a, b, b, sep='\n')
