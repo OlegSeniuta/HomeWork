b615e95dbf737ef5fb9e9009ab9a2e7eaf6dee71
OlegSeniuta <olegvemo@gmail.com>
2021-05-23 22:45:23 -0400

    commit1

---
+++ lesson_2/task2_2.py
@@ -0,0 +1,7 @@
+# Manipulate strings
+# Save your first and last name as separate variables, then
+# use string concatenation to add them together with a white space in between and print a greeting.
+first_name = 'Oleg'
+last_name = 'Seniuta'
+solv = first_name +' '+ last_name
+print(f'Welcome! {solv}')
---
+++ lesson_2/task2_3.py
@@ -0,0 +1,13 @@
+# Using python as a calculator.
+# Make a program with 2 numbers saved in separate
+# variables a and b, then print the result for each of the following:
+a = 12
+b = 44
+print(a + b)    # Addition
+print(a - b)    # Subtraction
+print(a / b)    # Division
+print(a * b)    # Multiplication
+print(a % b)    # Modulus
+print(a ** b)   # Exponent (Power)
+print(a // b)   # Floor division
+
